2799541af54dab3391335b3fe757e9914025c31b
BourdonAlexis <alexisbourdon@hotmail.fr>
2021-02-05 12:30:53 +0100

    ajout page contact

--- CV NUMERIQUE/assets/JS/script.js
+++ CV NUMERIQUE/assets/JS/script.js
@@ -1,94 +1,112 @@
-function checkForm(f) 
-{
-    formulaire=true;
+document.getElementById("Inscription").addEventListener("submit", function(e) {
 
-    //nom 
-    if (form1.nom.value=="")
-    {
-        nom.setCustomValidity("Veuillez saisir votre nom !");
-        return false;
-
-    }
-
-
-    //prénom
-    if (form1.prénom.value=="")
-    {
-        prénom.setCustomValidity("Veuillez saisir votre prénom !");   
-        return false;
-    }
-
-    //selection du sexe
-    if (form1.selectionsexe.value==false)
-    {
-        neutre.setCustomValidity("Veuillez saisir votre sexe !");
-        return false;
-    }
-    //date naissance
-    if (form1.date.value.charAt(7)=="")
-    {
-        date.setCustomValidity("Veuillez saisir une date de naissance valide !");
-        return false;
-    }
-    //code postal 
-    if (form1.codepostal.value.charAt(4)=="")
-    {
-        codepostal.setCustomValidity("Veuillez saisir votre code postal !");
-        return false;
-    }
-
-
-    //email
-    var email2 = document.getElementById('email').value;
-    var H = "@";
-    var cherche = email2.indexOf(H);
-    if(cherche==-1)
-    {
-        email.setCustomValidity("Veuillez saisir un email valide !");
-        return false;  
-    }
+    var erreur;
+     var sujet = document.getElementById("sujet");
+    var email = document.getElementById("email");
+    var question = document.getElementById("question");
     
-    //sujet
-   
-    if (sujet.value == "Choisir un sujet") {
-        sujet.setCustomValidity("Veuillez selectionner un sujet !");
-        return false;
+    
+    
+                          //*******************  sujet  *****************************
+    var error_sujet = document.getElementById("error_sujet");
+    var filtre_sujet = new RegExp("^[a-zA-Z]+$");
+    
+    console.log("sujet :" + sujet.value);
+    var resultat = filtre_sujet.test( sujet.value);
+    console.log(resultat);
+    
+    if (sujet.value==""){
+    
+        error_sujet.innerHTML="Veuillez renseigner votre sujet !!!!" .fontcolor("red"); 
+        e.preventDefault();
+    }
+    else{
+        if (resultat ==false) {
+            error_sujet.innerHTML="format incorrect" .fontcolor("red"); 
+            e.preventDefault();
         }
-        
+        else{
     
-    //question
-    var question2 = document.getElementById('question').value;
-    if(question2=="")
-    {
-        question.setCustomValidity("Veuillez saisir votre question !");
-        return false;    
-    }
-
-    //accepte formulaire
-    var validation = document.getElementById('traitementinformatique').checked;
-    if(validation==false)
-    {
-        traitementinformatique.setCustomValidity("Veuillez valider le traitement informatique !");
-        return false;        
+        error_sujet.innerHTML="";
+        }
     }
     
+    
 
-    //fin de check
-    if (formulaire==true)
-    {
-        return true;
+
+    
+    
+                        //********************* email ********************************
+    
+    var error_email = document.getElementById("error_email");
+    var filtre_email = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,4}$");
+    
+    console.log("email :" + email.value);
+    var resultat = filtre_email.test(email.value);
+    console.log(resultat);
+    
+    
+    if (email.value==""){
+    
+        error_email.innerHTML="Veuillez renseigner votre email !!!!" .fontcolor("red"); 
+        e.preventDefault();
     }
-    else 
-    {
-        alert("Le formulaire ne c'est pas envoyé correctement");
-        return false;
+    else{
+        if (resultat ==false) {
+            error_email.innerHTML="Email incorrect" .fontcolor("red"); 
+            e.preventDefault();
+        }
+        else{
+    
+        error_email.innerHTML="";
+        }
     }
+    
+      //************************* votre question ******************************
+
+var error_question = document.getElementById("error_question");
+var filtre_question = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+
+console.log("question :" + question.value);
+var resultat = filtre_question.test(question.value);
+console.log(resultat);
+
+if (question.value==""){
+
+    error_question.innerHTML="Veuillez poser votre question !!!!" .fontcolor("red"); 
+    e.preventDefault();
+    
 }
-// Affiche selection sujet en dehors du checkform
-sujet.addEventListener("change",function()
-{
-    if (sujet.value != "Choisir un sujet") 
-    {
-    question.value = sujet.value;
+
+
+    
+    //*******************************confirmation d'envoie*******************
+    
+    var error_formulaire = document.getElementById("error_formulaire");
+    console.log("formulaire :" + formulaire.checked);
+    console.log(resultat);
+    
+    if (formulaire.checked==""){
+    
+        error_formulaire.innerHTML="Veuillez poser votre question svp !!!!" .fontcolor("red"); 
+        e.preventDefault();
+        
     }
-});    
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
